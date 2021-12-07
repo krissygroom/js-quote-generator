5977b34bf32d4554793c9feb12f46f404dd0ed4d
krissy <krissydgroom@gmail.com>
2021-12-07 10:00:49 -0800

    Added quotes.js as local quote list option if api fails and accessed DOM elements to change quote text

---
+++ script.js
@@ -0,0 +1,53 @@
+// Select DOM elements
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+
+
+let apiQuotes = [];
+
+function newQuote() {
+    // Select a random quote from apiQuotes array
+    const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
+    // Check author field for null and change to 'unknown'
+    console.log(quote.text.length);
+    if (!quote.author) {
+        authorText.textContent = "Unknown";
+    } else {
+        authorText.textContent = quote['author'];
+    };
+    // Check quote length and apply long-quote class if needed
+    if (quote.text.length > 50) {
+        quoteText.classList.add('long-quote');
+    } else {
+        quoteText.classList.remove('long-quote');
+    }
+
+    quoteText.textContent = quote['text'];
+};
+
+// Get Quotes from API
+async function getQuotes() {
+    const apiUrl = 'https://type.fit/api/quotes';
+    try {
+        const response = await fetch(apiUrl);
+        // convert response into json object
+        // and store in global variable
+        apiQuotes = await response.json();
+    } catch (error) {
+        alert(error);
+        // Catch the error here if
+        // not able to fetch data
+        // and return a quote from quotes.js
+        apiQuotes = localQuotes;
+        console.log('Using localQuotes');
+    };
+    newQuote();
+};
+
+// On load, fetch quotes from above api
+getQuotes();
+
+
